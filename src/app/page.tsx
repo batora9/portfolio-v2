1f6a37b93bb379936a2f255b1057e980e6688a20
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-06-04 00:14:44 +0900

    枠組みを決める

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,22 @@ export default function Home() {
   return (
     <main>
       Init pages.
+      <div className={styles.profile}>
+        <h2>Profile</h2>
+      </div>
+      <div className={styles.skillsList}>
+        <h2>Skills</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Next.js</li>
+        </ul>
+      </div>
+      <div className={styles.worksList}>
+        <h2>Works</h2>
+      </div>
     </main>
   );
 }
