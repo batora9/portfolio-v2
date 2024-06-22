c47c92a68fae73b95bd7836ad873396fb5606b00
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-06-23 01:16:45 +0900

    ページの大まかな配置を決めた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,42 @@
+import clsx from "clsx";
 import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      Init pages.
-      <div className={styles.profile}>
-        <h2>Profile</h2>
+      <div className={styles.hero}>
+        <div className={styles.heroImage}></div>
       </div>
-      <div className={styles.skillsList}>
-        <h2>Skills</h2>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>Next.js</li>
-        </ul>
-      </div>
-      <div className={styles.worksList}>
-        <h2>Works</h2>
+      <div className={styles.contents}>
+        <div className={styles.profile}>
+          <div className={styles.profileContent}>
+            <h2>Profile</h2>
+            <p>Webエンジニアを目指している学生です。</p>
+          </div>
+        </div>
+        <div className={styles.skillsList}>
+          <div className={styles.skillListContent}>
+            <h2>Skills</h2>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>React</li>
+              <li>Next.js</li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.articlesList}>
+          <div className={styles.articlesListContent}>
+            <h2>Articles</h2>
+          </div>
+        </div>
+        <div className={styles.worksList}>
+          <div className={styles.worksListContent}>
+            <h2>Works</h2>
+          </div>
+        </div>
       </div>
     </main>
   );
