43c6863378ca48ceccbc5d1ae959bb00affa865d
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-06-23 03:35:41 +0900

    タイピング風テキスト作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { Header } from "../components/Header";
 import styles from "./page.module.css";
 
+
 export default function Home() {
   return (
     <main className={styles.main}>
---
+++ src/components/typing-text/typing-text.tsx
@@ -0,0 +1,13 @@
+import styles from './page.module.css';
+
+export const TypingText = () => {
+  return (
+    <main className={styles.main}>
+      <section className={styles.mainsection}>
+        <span>Typing string is cool!</span>
+      </section>
+    </main>
+  );
+}
+
+export default TypingText;
