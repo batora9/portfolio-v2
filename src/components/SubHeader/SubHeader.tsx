a0483fb16820b15032d2067030d05f35440b3742
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-07-02 11:11:56 +0900

    mainページ以外で使うヘッダーを追加

---
+++ src/components/SubHeader/SubHeader.tsx
@@ -0,0 +1,35 @@
+'use client';
+import React, { useEffect, useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Link from "next/link";
+import styles from "./SubHeader.module.css";
+
+export const SubHeader = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <header className={styles.header}>
+      <Link href="/" className={styles.logolink}>
+        <p className={styles.top}>ばとらの部屋</p>
+      </Link>
+      <nav className={`${styles.nav} ${menuOpen ? styles.open : ''}`}>
+        <ul>
+          <Link href="/" className={styles.link}>HOME</Link>
+          <Link href="./#profile" className={styles.link}>PROFILE</Link>
+          <Link href="./#skills" className={styles.link}>SKILLS</Link>
+          <Link href="/articles" className={styles.link}>ARTICLES</Link>
+          <Link href="/works" className={styles.link}>WORKS</Link>
+        </ul>
+      </nav>
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+    </header>
+  );
+};
+
+export default SubHeader;
