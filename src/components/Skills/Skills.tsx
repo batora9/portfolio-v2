6ff9abdb8c9c89a045989f555b6f276b318f4827
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-07-23 10:02:33 +0900

    skills iconを表示する

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -5,7 +5,18 @@ export function Skills() {
     <div className={styles.container}>
       <p className={styles.title}>Skills</p>
       <div className={styles.contents}>
-        主に、TypeScriptを使ったWebアプリケーションの開発や、C#を使ったWindowsアプリケーションの開発をしています。また、Goを使ったバックエンドやC++でアルゴリズムの勉強しています。
+        <div className={styles.langIcons}>
+          <img src="/icons/html5.svg" alt="html5" className={styles.icon} />
+          <img src="/icons/css3.svg" alt="css3" className={styles.icon} />
+          <img src="/icons/javascript.svg" alt="javascript" className={styles.icon} />
+          <img src="/icons/typescript.svg" alt="typescript" className={styles.icon} />
+          <img src="/icons/c-sharp.svg" alt="cs" className={styles.icon} />
+          <img src="/icons/cpp.svg" alt="cpp" className={styles.icon} />
+          <img src="/icons/git.svg" alt="git" className={styles.icon} />
+        </div>
+        <p>
+          主に、TypeScriptを使ったWebアプリケーションの開発や、C#を使ったWindowsアプリケーションの開発をしています。また、Goを使ったバックエンドやC++でアルゴリズムの勉強しています。
+        </p>
       </div>
     </div>
   );
