a0127c77a31367a50b8507e715b5a8c88b99503e
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-10-16 06:13:46 +0900

    プロフィールを手直し

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -10,12 +10,17 @@ export function Skills() {
           <img src="/icons/css3.svg" alt="css3" className={styles.icon} />
           <img src="/icons/javascript.svg" alt="javascript" className={styles.icon} />
           <img src="/icons/typescript.svg" alt="typescript" className={styles.icon} />
+          <img src="/icons/go.png" alt="go" className={styles.icon} />
           <img src="/icons/c-sharp.svg" alt="cs" className={styles.icon} />
           <img src="/icons/cpp.svg" alt="cpp" className={styles.icon} />
           <img src="/icons/git.svg" alt="git" className={styles.icon} />
         </div>
         <p>
-          主に、TypeScriptを使ったWebアプリケーションの開発や、C#を使ったWindowsアプリケーションの開発をしています。また、Goを使ったバックエンドやC++でアルゴリズムの勉強しています。
+          主にReactを用いたフロントエンドの開発を得意としています。バックエンドは個人開発でGoを使用した経験があります。(Web開発が中心)
+        </p>
+        <p>
+          サークル活動を通して、競技プログラミング(Algorithms)に取り組んだりメンバーとGitHubを用いた共同開発を行うなど、チーム開発においても経験を積んでいます。
+          気まぐれにC#でアプリケーションを作ったり、セキュリティ分野に手を出してみたりしています。
         </p>
       </div>
     </div>
