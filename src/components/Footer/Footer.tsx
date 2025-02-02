459aff0dc7d0c463b5321add58e1f9dc02dc8dd6
batora9 <sm.net_minecraft.hacker@outlook.jp>
2025-02-02 23:24:48 +0900

    タイムラインのデザインを変える

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>© 2023-2024 batora</p>
+      <p>© 2023-2025 batora</p>
       <p>All Rights Reserved.</p>
     </footer>
   );
