4ecbcd0db6e2c244564f0d81f9bd40c673744a58
batora9 <sm.net_minecraft.hacker@outlook.jp>
2025-02-27 23:17:14 +0900

    経歴を追加

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -4,36 +4,42 @@ export const Timeline = () => {
   // Timelineのデータ
   const data = [
     {
+      title: "株式会社リチェルカ",
+      date: "2025年2月-現在",
+      description: "学生インターンとしてフルスタックで販売・仕入・在庫管理SCM SaaS『RECERQA』の開発に携わる",
+      icon: "/icons/recerqa.png",
+    },
+    {
       title: "ICPC 2024 国内予選に出場",
-      date: "2024-07",
+      date: "2024年7月",
       description: "チームMAXIMUM TUNE 6RRで出場し結果は298位",
       icon: "/icons/icpc.png",
     },
     {
       title: "ICPC 2023 国内予選に出場",
-      date: "2023-07",
+      date: "2023年7月",
       description: "チームtouch_hogehogeで出場し結果は174位",
       icon: "/icons/icpc.png",
     },
     {
       title: "プログラミングサークルMaximumに入会",
-      date: "2023-05",
+      date: "2023年5月-現在",
       description: "2024年度副会長, 2025年度会長とWeb研究会の講師を務める",
       icon: "/icons/maximum.png",
     },
     {
       title: "埼玉大学に進学",
-      date: "2023-04",
+      date: "2023年4月-現在",
       description: "埼玉大学工学部情報工学科",
       icon: "/icons/saitama-u.png",
     },
     {
       title: "実用英語技能検定2級を取得",
-      date: "2022-11",
+      date: "2022年11月",
     },
     {
       title: "茨城県立土浦第一高等学校を卒業",
-      date: "2022-03",
+      date: "2022年3月",
       description: "部活動では硬式野球部の副主将を務める",
       icon: "/icons/tsuchiura-first.png",
     },
